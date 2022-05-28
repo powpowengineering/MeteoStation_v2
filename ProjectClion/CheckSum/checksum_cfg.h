9503d499c7f490ef1b95765ad63f7ed5295a0274
Kutuzov Pavel <ku2zovpavel@gmail.com>
2022-05-28 09:12:39 +0300

    Added crc module

---
+++ ProjectClion/CheckSum/checksum_cfg.h
@@ -0,0 +1,40 @@
+//**************************************************************************************************
+// @Module        CH_SUM
+// @Filename      checksum_cfg.h
+//--------------------------------------------------------------------------------------------------
+// @Description   Configuration of the required functionality of the CH_SUM module.
+//
+//--------------------------------------------------------------------------------------------------
+// @Version       1.0.0
+//--------------------------------------------------------------------------------------------------
+// @Date          XX.XX.XXXX
+//--------------------------------------------------------------------------------------------------
+// @History       Version  Author      Comment
+// XX.XX.XXXX     1.0.0    KPS         First release.
+//**************************************************************************************************
+
+#ifndef CH_SUM_CFG_H
+#define CH_SUM_CFG_H
+
+
+
+//**************************************************************************************************
+// Definitions of global (public) constants
+//**************************************************************************************************
+
+// Enable/disable the development error detection feature of the program module.
+// Used only for debug purposes and should be disabled in the final release.
+// Valid values: ON / OFF
+#define MODULE_DEVELOPMENT_ERROR_DETECTION      (OFF)
+
+// User can enable/disable the internal diagnostic of the program module.
+// Used only for debug purposes and should be disabled in the final release.
+// Valid values: ON / OFF
+#define MODULE_INTERNAL_DIAGNOSTICS             (OFF)
+
+// None.
+
+
+#endif // #ifndef AM2305_CFG_H
+
+//****************************************** end of file *******************************************
