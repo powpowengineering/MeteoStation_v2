3ba49d0464b8daa6da4c4f83c11da1ef9587ec71
Kutuzov Pavel <ku2zovpavel@gmail.com>
2022-05-28 22:27:56 +0300

    Added Test 1, Test 2

--- ProjectClion/Users/src/task_test_flash.c
+++ ProjectClion/Users/src/task_test_flash.c
@@ -114,10 +114,14 @@ uint8_t dataWrite[SIZE_BUF];
 //**************************************************************************************************
 void vTaskTestFlash(void *pvParameters)
 {
-    STD_RESULT result = RESULT_NOT_OK;
     uint64_t ID;
     uint16_t ManufID;
     uint32_t adr=0;
+    uint16_t nNumberOfPass=0;
+    uint16_t nNumberOfFail=0;
+    uint16_t nNumberOfEmpty=0;
+    uint16_t nNumberOfNotEmpty=0;
+    uint32_t nSizeRead=0;
 
     W25Q_Init();
 
@@ -128,17 +132,24 @@ void vTaskTestFlash(void *pvParameters)
 
     printf("Manufacture ID %d\n\r",(uint8_t)(ManufID>>8));
 
-    // Test 1: Erase three random 4k sectors and write and read. Address multiple of 4K.
-
+    // Test 1: Erase/write/read random number of 4k sectors. Address multiple of 4K.
+    printf("Start Test 1:\n\r");
     // erase/write/read/check data
-    result = RESULT_OK;
     for (int i=0;i<TEST_FLASH_NUMBER_OF_ITERATIONS_TEST_1;i++)
     {
         // get address
         adr = (rand() / W25Q_QTY_SECTORS) * W25Q_CAPACITY_SECTOR_BYTES;
+        printf("Address %x; ",adr);
 
         // Erase sector 4K
-        result |= W25Q_EraseBlock(adr,W25Q_BLOCK_MEMORY_4KB);
+        if (RESULT_OK == W25Q_EraseBlock(adr,W25Q_BLOCK_MEMORY_4KB))
+        {
+            printf("Erase 4K PASS; ");
+        }
+        else
+        {
+            printf("Erase 4K FAIL; ");
+        }
 
         // Prepare dataWrite
         for(int j=0;j<SIZE_BUF-1;j++)
@@ -148,33 +159,100 @@ void vTaskTestFlash(void *pvParameters)
         dataWrite[SIZE_BUF-1] = CH_SUM_CalculateCRC8(dataWrite, SIZE_BUF-1);
 
         // Write sector 4K
-        result |= W25Q_WriteData(adr,dataWrite,SIZE_BUF);
+        if (RESULT_OK == W25Q_WriteData(adr,dataWrite,SIZE_BUF))
+        {
+            printf("Write 4K PASS; ");
+        }
+        else
+        {
+            printf("Write 4K FAIL; ");
+        }
 
         // Read sector 4K
-        result |=  W25Q_ReadData(adr,dataRead, SIZE_BUF);
+        if (RESULT_OK == W25Q_ReadData(adr,dataRead, SIZE_BUF))
+        {
+            printf("Read 4K PASS");
+        }
+        else
+        {
+            printf("Read 4K FAIL");
+        }
 
         // Check data
-        if (dataRead[SIZE_BUF-1] != CH_SUM_CalculateCRC8(dataRead, SIZE_BUF-1))
+        if (dataRead[SIZE_BUF-1] == CH_SUM_CalculateCRC8(dataRead, SIZE_BUF-1))
         {
-            result = RESULT_NOT_OK;
+            printf("Check Sum PASS; TEST 1.%d PASS\r\n",i);
+            nNumberOfPass++;
+        }
+        {
+            printf("Check Sum FAIL; TEST 1.%d FAIL\r\n",i);
+            nNumberOfFail++;
         }
     }
-    if (RESULT_OK == result)
-    {
-        printf("Test 1 PASS\n\r");
-    }
-    else
-    {
-        printf("Test 1 FAIL\n\r");
-    }
+    printf("Finish Test 1: Tests passed %d ; Tests failed %d\n\r",nNumberOfPass,nNumberOfFail);
+    printf("\r\n");
     // End Test 1
 
     // Test 2: Erase Chip
-    W25Q_EraseBlock(adr,    W25Q_BLOCK_MEMORY_ALL);
+    // Read all sectors
+    for (uint8_t step=0;step<2;step++)
+    {
+        adr = 0;
+        nSizeRead = SIZE_BUF&(W25Q_CAPACITY_SECTOR_BYTES-1);
+        nNumberOfNotEmpty = 0;
 
+        for (int i=0;i<W25Q_QTY_SECTORS;i++)
+        {
+            // Read next sector 4K
+            for (uint32_t cntBytes=0; cntBytes < W25Q_CAPACITY_SECTOR_BYTES;)
+            {
+                if (RESULT_OK == W25Q_ReadData(adr,dataRead, nSizeRead))
+                {
+                    for (int j=0; j<nSizeRead; j++)
+                    {
+                        if (0xff != dataRead[j])
+                        {
+                            nNumberOfNotEmpty++;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    printf("Error reading sector address %x\r\n",adr);
+                    break;
+                }
+                cntBytes += nSizeRead;
+                adr += nSizeRead;
+            }
+        }
+        printf("Number of not empty sectors  %d\r\n",nNumberOfNotEmpty);
 
+        if (0 == step)
+        {
+            // Erase chip
+            printf("Chip erasing ...\r\n");
+            if (RESULT_OK == W25Q_EraseBlock(0,    W25Q_BLOCK_MEMORY_ALL))
+            {
+                printf("Chip erasing was successful\r\n");
+            }
+            else
+            {
+                printf("Chip erase error\r\n");
+            }
+        }
+    }
+    if ( 0 == nNumberOfNotEmpty )
+    {
+        printf("Finish Test 2: Tests passed \n\r");
+    }
+    else
+    {
+        printf("Finish Test 2: Tests failed; Number of not empty sectors %d\n\r",nNumberOfNotEmpty);
+    }
 
-
+    printf("\r\n");
+    // End Test 2
 
 
     while(1)
