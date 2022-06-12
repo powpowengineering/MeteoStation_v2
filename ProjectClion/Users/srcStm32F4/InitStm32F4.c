931fe0fe9e127d0c38393a8d71e408909e17d273
nemasha42 <maltseva@gmail.com>
2022-06-12 16:43:26 +0300

    Added test e-paper

--- ProjectClion/Users/srcStm32F4/InitStm32F4.c
+++ ProjectClion/Users/srcStm32F4/InitStm32F4.c
@@ -5,23 +5,76 @@ void InitStm32F4(void)
 {
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
 
-    GPIO_InitTypeDef GPIO_InitStruct;
+    // USART1
+      GPIO_InitTypeDef GPIO_InitStruct;
+//    GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_9;
+//    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+//    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+//    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+//    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+//    GPIO_Init(GPIOA, &GPIO_InitStruct);
+//    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
+//    // USART1
+//    GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_10;
+//    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+//    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+//    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+//    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+//    GPIO_Init(GPIOA, &GPIO_InitStruct);
+//    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
+
+    // SPI1 CLK
+    GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_5;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource5, GPIO_AF_SPI1);
+
+    // SPI1 MOSI
+    GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_7;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource7, GPIO_AF_SPI1);
+
+    // SPI1 CS
+    GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_6;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    // DC E-LNK
+    GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_8;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    // RST E-LNK
     GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_9;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOA, &GPIO_InitStruct);
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
 
+    // BUSY E-LNK
     GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_10;
-    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
     GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
     GPIO_Init(GPIOA, &GPIO_InitStruct);
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
 
     USART_InitTypeDef USART_InitStruct;
     USART_InitStruct.USART_BaudRate = 115200;
@@ -32,7 +85,41 @@ void InitStm32F4(void)
     USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 
     USART_Init(USART1, &USART_InitStruct);
-    USART_Cmd(USART1, ENABLE);
+    USART_Cmd(USART1, DISABLE);
+
+    // SPI Init
+    SPI_InitTypeDef SPI_InitStruct;
+    /* Initialize the SPI_Direction member */
+    SPI_InitStruct.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+    /* initialize the SPI_Mode member */
+    SPI_InitStruct.SPI_Mode = SPI_Mode_Master;
+    /* initialize the SPI_DataSize member */
+    SPI_InitStruct.SPI_DataSize = SPI_DataSize_8b;
+    /* Initialize the SPI_CPOL member */
+    SPI_InitStruct.SPI_CPOL = SPI_CPOL_Low;
+    /* Initialize the SPI_CPHA member */
+    SPI_InitStruct.SPI_CPHA = SPI_CPHA_1Edge;
+    /* Initialize the SPI_NSS member */
+    SPI_InitStruct.SPI_NSS = SPI_NSS_Soft;
+    /* Initialize the SPI_BaudRatePrescaler member */
+    SPI_InitStruct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
+    /* Initialize the SPI_FirstBit member */
+    SPI_InitStruct.SPI_FirstBit = SPI_FirstBit_MSB;
+    /* Initialize the SPI_CRCPolynomial member */
+    SPI_InitStruct.SPI_CRCPolynomial = 7;
+
+    SPI_Init(SPI1, &SPI_InitStruct);
+    SPI_Cmd(SPI1, ENABLE);
+
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+    TIM_TimeBaseInitStruct.TIM_Period = 0xFFFF;
+    TIM_TimeBaseInitStruct.TIM_Prescaler = 42000-1;
+    TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0x0000;
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
+    TIM_Cmd(TIM2, ENABLE);
+
 }
 
 
