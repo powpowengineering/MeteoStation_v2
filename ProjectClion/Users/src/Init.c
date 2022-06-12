1c063777cde5e48e64458534f4ed9ed1786a324b
nemasha42 <maltseva@gmail.com>
2022-06-12 19:29:31 +0300

    Added init bme280

--- ProjectClion/Users/src/Init.c
+++ ProjectClion/Users/src/Init.c
@@ -153,6 +153,7 @@ void Init(void)
     RCC_APB2PeriphClockCmd(    RCC_APB2Periph_ADC1, ENABLE); //activated ADC
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
     RCC_APB1PeriphClockCmd(    RCC_APB1Periph_USART3, ENABLE);
+    RCC_APB1PeriphClockCmd(    RCC_APB1Periph_I2C1, ENABLE);
 
 
     /*GPIO Init*/
@@ -260,6 +261,22 @@ void Init(void)
 
     ADC_Cmd(ADC1, ENABLE);
 
+    I2C_InitTypeDef I2C_InitStruct;
+
+/*---------------- Reset I2C init structure parameters values ----------------*/
+        /* initialize the I2C_ClockSpeed member */
+        I2C_InitStruct.I2C_ClockSpeed = 5000;
+        /* Initialize the I2C_Mode member */
+        I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
+        /* Initialize the I2C_DutyCycle member */
+        I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
+        /* Initialize the I2C_OwnAddress1 member */
+        I2C_InitStruct.I2C_OwnAddress1 = 0;
+        /* Initialize the I2C_Ack member */
+        I2C_InitStruct.I2C_Ack = I2C_Ack_Disable;
+        /* Initialize the I2C_AcknowledgedAddress member */
+        I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+        I2C_Init(I2C1, &I2C_InitStruct);
 
 
 }
